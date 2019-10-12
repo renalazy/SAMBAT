f2a59272faf0247d14f3ec3246f2c3c80abe7fd1
renaldi <renalazyy@gmail.com>
2019-10-12 23:38:55 +0700

    Register activity & layout commit

--- app/src/main/java/com/example/sambat/RegisterActivity.java
+++ app/src/main/java/com/example/sambat/RegisterActivity.java
@@ -11,4 +11,10 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
+
+    @Override
+    public void onBackPressed()
+    {
+        finish();
+    }
 }
