c95bbfddb63642fceceefa5bda1fcf06f439a87e
renaldi <renalazyy@gmail.com>
2019-10-12 23:23:53 +0700

    updated onbackpressed methods

--- app/src/main/java/com/example/sambat/RegisterActivity.java
+++ app/src/main/java/com/example/sambat/RegisterActivity.java
@@ -4,17 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
-
-    @Override
-    public void onBackPressed()
-    {
-        finish();
+        setContentView(R.layout.activity_register);
     }
 }
